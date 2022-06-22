935bad57ad9eeb93627079b35a34a6dcd0b7ff91
n-jabes <nshutij7@gmail.com>
2022-06-22 22:23:26 +0200

    positioned the navbar to the center

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import Navbar from "../../components/navbar/Navbar"
 import Sidebar from "../../components/sidebar/Sidebar"
 import "./home.scss"
 
@@ -6,7 +7,7 @@ const Home = () => {
     <div className="home">
       <Sidebar />
       <div className="homeContainer">
-        container
+        <Navbar />
       </div>
     </div>
   )

6ab34aabf53f290b5b9f453964d31b021ca6f1a5
n-jabes <nshutij7@gmail.com>
2022-06-24 20:14:43 +0200

    working on the widgets on the home container

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../../components/navbar/Navbar"
 import Sidebar from "../../components/sidebar/Sidebar"
+import Widget from "../../components/widget/Widget"
 import "./home.scss"
 
 const Home = () => {
@@ -8,6 +9,12 @@ const Home = () => {
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
+        <div className="widgets">
+          <Widget/>
+          <Widget/>
+          <Widget/>
+          <Widget/>
+        </div>
       </div>
     </div>
   )

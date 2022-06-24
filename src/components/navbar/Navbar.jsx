565067dbb60a6529032741282bb79ecf47832a46
n-jabes <nshutij7@gmail.com>
2022-06-24 18:29:33 +0200

    finished the navbar initiation, next is stlyling it.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,44 @@
 import "./navbar.scss"
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
+
 
 const Navbar = () => {
   return (
-    <div className="navbar">Navbar</div>
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <SearchIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageIcon/>
+            English
+          </div>
+          <div className="item">
+            <DarkModeIcon/>
+          </div>
+          <div className="item">
+            <FullscreenExitIcon/>
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon/>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon/>
+          </div>
+          <div className="item">
+            <ListOutlinedIcon/>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,6 +8,7 @@ const Home = () => {
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
+        home container
       </div>
     </div>
   )

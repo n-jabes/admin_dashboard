b350ff4b816c24245409eab698b5db1e737049be
Nshuti Jabes <nshutij7@gmail.com>
2022-07-11 22:08:03 +0200

    working on the progress bar section on the home page

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,5 +1,7 @@
 import "./featured.scss"
 import MoreVertIcon from "@mui/icons-material/MoreVert"
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 function Featured() {
   return (
@@ -8,7 +10,14 @@ function Featured() {
         <div className="title">Total Revenue</div>
         <MoreVertIcon fontSize="small" />
       </div>
-      <div className="bottom"></div>
+      <div className="bottom">
+        <div className="featuredChart">
+          <CircularProgressbar value={70} text={"70%"} strokeWidth={5}/>
+        </div>
+        <p className="title">Total sales made today</p>
+        <p className="amouynt">$420</p>
+        <p className="desc">Previous transactions processing. Last payments may not be included.</p>
+      </div>
     </div>
     )
 }

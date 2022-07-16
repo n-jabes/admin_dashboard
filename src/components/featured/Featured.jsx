c3c512de9bb1bae83629254eccda135ccbb5e3b3
Nshuti Jabes <nshutij7@gmail.com>
2022-07-16 21:29:14 +0200

    working on the summary section of the featured chart on thehome page

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -18,6 +18,15 @@ function Featured() {
         <p className="amouynt">$420</p>
         <p className="desc">Previous transactions processing. Last payments may not be included.</p>
       </div>
+      <div className="summary">
+        <div className="item">
+          <div className="itemTitle">Target</div>
+            <div className="itemResult">
+              
+              <div className="resultAmount">$12.4k</div>
+            </div>
+        </div>
+      </div>
     </div>
     )
 }

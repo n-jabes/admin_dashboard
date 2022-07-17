2a4586c7fef51e87ee2a7ef8bc39d9bdc30b9ef8
Nshuti Jabes <nshutij7@gmail.com>
2022-07-17 18:36:53 +0200

    finished working on the featured chart on the homepage

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -2,6 +2,8 @@ import "./featured.scss"
 import MoreVertIcon from "@mui/icons-material/MoreVert"
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown"
+import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
 
 function Featured() {
   return (
@@ -20,11 +22,25 @@ function Featured() {
       </div>
       <div className="summary">
         <div className="item">
-          <div className="itemTitle">Target</div>
-            <div className="itemResult">
-              
-              <div className="resultAmount">$12.4k</div>
-            </div>
+          <div className="itemTitle">Yesterday</div>
+          <div className="itemResult negative">
+            <KeyboardArrowDownIcon />
+            <div className="resultAmount">$12.4k</div>
+          </div>
+        </div>
+        <div className="item">
+          <div className="itemTitle">Last Week</div>
+          <div className="itemResult positive">
+            <KeyboardArrowUpOutlinedIcon />
+            <div className="resultAmount">$12.4k</div>
+          </div>
+        </div>
+        <div className="item">
+          <div className="itemTitle">Last Month</div>
+          <div className="itemResult positive">
+            <KeyboardArrowUpOutlinedIcon />
+            <div className="resultAmount">$12.4k</div>
+          </div>
         </div>
       </div>
     </div>

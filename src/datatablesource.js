cc052bbabc0155afd853c67a479aad2cee4b9d56
Nshuti Jabes <nshutij7@gmail.com>
2022-07-26 11:51:02 +0200

    added a file containing temporary data to be used as a data source for the users list page

---
+++ src/datatablesource.js
@@ -0,0 +1,84 @@
+//temporary data
+
+export const userRows = [
+    {
+        id: 1,
+        username: "Snow",
+        img: "https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        status: "active",
+        email: "1snow@gmail.com",
+        age: 35
+    },
+    {
+        id: 2,
+        username: "Jamie Lannister",
+        img: "https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        status: "passive",
+        email: "2snow@gmail.com",
+        age: 42
+    },
+    {
+        id: 3,
+        username: "Lannister",
+        img: "https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        status: "pending",
+        email: "3snow@gmail.com",
+        age: 45
+    },
+    {
+        id: 4,
+        username: "Stark",
+        img: "https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        status: "active",
+        email: "4snow@gmail.com",
+        age: 16
+    },
+    {
+        id: 5,
+        username: "Targaryen",
+        img: "https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        status: "passive",
+        email: "5snow@gmail.com",
+        age: 22
+    },
+    {
+        id: 6,
+        username: "Melisandre",
+        img: "https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        status: "active",
+        email: "6snow@gmail.com",
+        age: 15
+    },
+    {
+        id: 7,
+        username: "Clifford",
+        img: "https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        status: "passive",
+        email: "7snow@gmail.com",
+        age: 44
+    },
+    {
+        id: 8,
+        username: "Frances",
+        img: "https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        status: "active",
+        email: "8snow@gmail.com",
+        age: 36
+    },
+    {
+        id: 9,
+        username: "Roxie",
+        img: "https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        status: "pending",
+        email: "9snow@gmail.com",
+        age: 65
+    },
+    {
+        id: 10,
+        username: "Roxie",
+        img: "https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        status: "active",
+        email: "10snow@gmail.com",
+        age: 35
+    },
+];
--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,8 +1,17 @@
 import "./list.scss"
+import SideBar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar"
+import DataTable from "../../components/datatable/DataTable"
 
 const List = () => {
   return (
-    <div>List</div>
+    <div className="list">
+      <SideBar/>
+      <div className="listContainer">
+        <Navbar/>
+        <DataTable/>
+      </div>
+    </div>
   )
 }
 

db0bc7c5f1bd366ae163c142375caeb8ad809999
Nshuti Jabes <nshutij7@gmail.com>
2022-07-26 19:58:04 +0200

    working on the user table on the list page

--- src/datatablesource.js
+++ src/datatablesource.js
@@ -1,5 +1,41 @@
-//temporary data
+export const userColumns = [
+    {field: "id", headerName: "ID", width: 70},
+    {
+        field: "username", 
+        headerName: "User", 
+        width: 230, 
+        renderCell: (params) => {
+            return (
+                <div className="cellWithImg">
+                    <img src={params.row.img} alt="avatar" className="cellImg" />
+                    {params.row.username}
+                </div>
+            );
+        } 
+    },
+    {
+        field: "email",
+        headerName: "Email",
+        width: 230,
+    },
+    {
+        field: "age",
+        headerName: "Age",
+        width: 100
+    },
+    {
+        field: "status",
+        headerName: "Status",
+        width: 160,
+        renderCell: (params) => {
+            return (
+                <div className={`cellWithStatus ${params.row.status}`}>{params.row.status}</div>
+            );
+        }
+    }
+];
 
+//temporary data
 export const userRows = [
     {
         id: 1,

d857b6a37b363b789c0732c07680de582b1e1e85
Nshuti Jabes <nshutij7@gmail.com>
2022-07-28 09:25:43 +0200

    finished building the actions column which will be used on all the table lists either for users or products

--- src/components/datatable/DataTable.jsx
+++ src/components/datatable/DataTable.jsx
@@ -3,12 +3,27 @@ import { DataGrid } from '@mui/x-data-grid';
 import { userRows, userColumns } from "../../datatablesource"
 
 function DataTable() {
+  const actionColumns = [
+    {
+      field: "action", 
+      headerName: "Actions",
+      width: 200,
+      renderCell: () => {
+        return(
+          <div className="cellActions">
+            <div className="viewButton">view</div>
+            <div className="deleteButton">delete</div>
+          </div>
+        );
+      }
+  }
+  ]
   return (
     <DataGrid className="datatable"
         rows={userRows}
-        columns={userColumns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
+        columns={userColumns.concat(actionColumns)}
+        pageSize={8}
+        rowsPerPageOptions={[8]}
         checkboxSelection
       />
   )

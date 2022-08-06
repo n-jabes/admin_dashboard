d2c2bd2360bf0b9a7c12106ff9b46c074c3b7da9
Nshuti Jabes <nshutij7@gmail.com>
2022-08-06 20:44:29 +0200

    working on the list of latest transactions made by a particular user

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -2,6 +2,7 @@ import "./single.scss"
 import Sidebar from "../../components/sidebar/Sidebar"
 import Navbar from "../../components/navbar/Navbar"
 import Chart from "../../components/chart/Chart"
+import List from "../../components/table/Table"
 
 const Single = () => {
   return (
@@ -44,7 +45,10 @@ const Single = () => {
             <Chart aspect={3 / 1} title="User Spendings ( Last 6 Months)"/>
           </div>
         </div>
-        <div className="bottom"></div>
+        <div className="bottom">
+          <h1 className="title">Last transactions</h1>
+          <List />
+        </div>
       </div>
     </div>
   )

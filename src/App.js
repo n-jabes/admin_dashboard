a528b31f63b5f74018baeff1ef93dcc1a1cad2ea
Nshuti Jabes <nshutij7@gmail.com>
2022-08-07 21:25:52 +0200

    added the logic which we will use to display the image selected by the user to the left section of the add new user/product page and we are now done with the add new user/product page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import List from "./pages/list/List";
 import New from "./pages/new/New";
 import Single from "./pages/single/Single";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { productInputs, userInputs } from "./formSource";
 
 
 function App() {
@@ -17,12 +18,12 @@ function App() {
             <Route path="users">
               <Route index element={ <List /> }/>
               <Route path=":userId" element={ <Single /> }/>
-              <Route path="new" element={ <New /> }/>
+              <Route path="new" element={ <New inputs = {userInputs} title="Add New User"/> }/>
             </Route>
             <Route path="products">
               <Route index element={ <List /> }/>
               <Route path=":productId" element={ <Single /> }/>
-              <Route path="new" element={ <New /> }/>
+              <Route path="new" element={ <New inputs={productInputs} title="Add new Product" /> }/>
             </Route>
           </Route>
         </Routes>
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -2,19 +2,30 @@ import "./new.scss"
 import Sidebar from "../../components/sidebar/Sidebar"
 import Navbar from "../../components/navbar/Navbar"
 import { DriveFolderUploadOutlinedIcon } from "@mui/icons-material/DriveFolderUploadOutlined"
+import { useState } from "react"
 
-const New = () => {
+const New = ({inputs, title}) => {
+
+  const [file, setFile] = useState("");
+
   return (
     <div className="new">
       <Sidebar />
       <div className="newContainer">
         <Navbar />
         <div className="top">
-          <h1>Add New User</h1>
+          <h1>{title}</h1>
         </div>
         <div className="bottom">
           <div className="left">
-            <img src="https:/ / icon-library.com/images/no-image-icon/ no-image-icon-0.jpg" alt="" />
+            <img 
+              src={
+                file 
+                  ? URL.createObjectURL(file) 
+                  : "https:/ / icon-library.com/images/no-image-icon/ no-image-icon-0.jpg"
+              } 
+              alt="" 
+            />
           </div>
           <div className="right">
             <form>
@@ -22,36 +33,22 @@ const New = () => {
                 <label htmlFor="file">
                   Image: <DriveFolderUploadOutlinedIcon className="icon" />
                 </label>
-                <input type="file" id="file" style={{display: none}}/>
+                <input 
+                  type="file" 
+                  id="file" 
+                  onChange={e => setFile(e.target.files[0]) } 
+                  style={{display: none}}
+                />
               </div>
-              <div className="formInput">
-                <label>Username</label>
-                <input type="text"  placeholder="john_doe"/>
+
+              {inputs.map((input) => (
+                <div className="formInput" key={input.id}>
+                <label>{input.label}</label>
+                <input type={input.type} placeholder={input.placeholder}/>
               </div>
-              <div className="formInput">
-                <label>Name and Surname</label>
-                <input type="text"  placeholder="John Doe"/>
-              </div>
-              <div className="formInput">
-                <label>Email</label>
-                <input type="email"  placeholder="john_doe@gmail.com"/>
-              </div>
-              <div className="formInput">
-                <label>Phone</label>
-                <input type="text"  placeholder="+1 234 567 89"/>
-              </div>
-              <div className="formInput">
-                <label>Password</label>
-                <input type="password"/>
-              </div>
-              <div className="formInput">
-                <label>Address</label>
-                <input type="text"  placeholder="Elton St 216 NewYork"/>
-              </div>
-              <div className="formInput">
-                <label>Country</label>
-                <input type="text"  placeholder="USA"/>
-              </div>
+              ))}
+              
+             
               <button>Send</button>
             </form>
           </div>

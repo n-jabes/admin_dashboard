249a79ee012b8c365cc265369ce70f244f4b6b92
Nshuti Jabes <nshutij7@gmail.com>
2022-08-07 20:30:40 +0200

    added an unstyled form to the right section of the add new user page besides the user image section

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -13,7 +13,38 @@ const New = () => {
           <div className="left">
             <img src="https:/ / icon-library.com/images/no-image-icon/ no-image-icon-0.jpg" alt="" />
           </div>
-          <div className="right">right</div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label>Username</label>
+                <input type="text"  placeholder="john_doe"/>
+              </div>
+              <div className="formInput">
+                <label>Name and Surname</label>
+                <input type="text"  placeholder="John Doe"/>
+              </div>
+              <div className="formInput">
+                <label>Email</label>
+                <input type="email"  placeholder="john_doe@gmail.com"/>
+              </div>
+              <div className="formInput">
+                <label>Phone</label>
+                <input type="text"  placeholder="+1 234 567 89"/>
+              </div>
+              <div className="formInput">
+                <label>Password</label>
+                <input type="password"/>
+              </div>
+              <div className="formInput">
+                <label>Address</label>
+                <input type="text"  placeholder="Elton St 216 NewYork"/>
+              </div>
+              <div className="formInput">
+                <label>Country</label>
+                <input type="text"  placeholder="USA"/>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>

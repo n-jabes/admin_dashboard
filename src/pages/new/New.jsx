3a7e5fe22926ae33fa50d8b10e89893b578c7dc5
Nshuti Jabes <nshutij7@gmail.com>
2022-08-07 20:23:01 +0200

    initiated the new user page, finished working on the left section which will contain the user image.

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -2,7 +2,21 @@ import "./new.scss"
 
 const New = () => {
   return (
-    <div>New</div>
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1>Add New User</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img src="https:/ / icon-library.com/images/no-image-icon/ no-image-icon-0.jpg" alt="" />
+          </div>
+          <div className="right">right</div>
+        </div>
+      </div>
+    </div>
   )
 }
 

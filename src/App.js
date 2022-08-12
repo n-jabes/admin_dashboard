666d7ea2f90384c82bab96845b3cfb6884072e38
Nshuti Jabes <nshutij7@gmail.com>
2022-08-12 20:43:46 +0200

    working on the dark mode using context and reducer

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ import Single from "./pages/single/Single";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { productInputs, userInputs } from "./formSource";
 import "./style/dark.scss"
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext";
 
 function App() {
+
+const {darkMode} = useContext (DarkModeContext)
+
   return (
-    <div className="app dark">
+    <div className={darkMode ? "app dark" : "app"}>
       <BrowserRouter>
         <Routes>
           <Route path="/">
